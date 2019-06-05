a679b78372d5abde70bddbe4e68289fc1e47bbd8
monikanikolova <monika.nikolova@yahoo.com>
2019-06-05 11:46:23 -0400

    inital commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+// import Cards from "./components/Cards";
+// import Title from "./components/Title";
+// import Wrapper from "./components/Wrapper";
+// import pictures from "./pictures.json";
+
+class App extends Component {
+    render() {
+        return (
+
+        )
+    }
+}
+export default App;
