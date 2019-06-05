8c1a1800470152918356f5a4390274cafb737742
monikanikolova <monika.nikolova@yahoo.com>
2019-06-05 12:20:43 -0400

    Added Title Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 // import Cards from "./components/Cards";
-// import Title from "./components/Title";
+import Title from "./components/Title";
 import Wrapper from "./components/Wrapper";
 // import pictures from "./pictures.json";
 
 class App extends Component {
-    render() {
-        return (
-          <Wrapper>
     
-          </Wrapper>
-        )
-        }
+  render(){
+    return (
+      <Wrapper>
+        <div className="header">
+            <Title>
+              Emojis Gone Wild
+            </Title>
+          </div>
+      </Wrapper>
+    )
+  }
 }
+
+
 export default App;
