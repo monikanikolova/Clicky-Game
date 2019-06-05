4769ea4f14b103e4fcb1ab31c874f1e5a518eff1
monikanikolova <monika.nikolova@yahoo.com>
2019-06-05 12:01:05 -0400

    Added Wrapper component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 // import Cards from "./components/Cards";
 // import Title from "./components/Title";
-// import Wrapper from "./components/Wrapper";
+import Wrapper from "./components/Wrapper";
 // import pictures from "./pictures.json";
 
 class App extends Component {
+    render() {
+        return (
+          <Wrapper>
     
+          </Wrapper>
+        )
+        }
 }
 export default App;
