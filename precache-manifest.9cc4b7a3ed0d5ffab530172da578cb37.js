dc6c321994dc611a963295d77228a75be0565b1e
monikanikolova <monika.nikolova@yahoo.com>
2019-06-08 09:53:30 -0400

    Updates

---
+++ precache-manifest.9cc4b7a3ed0d5ffab530172da578cb37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3aee99a6d79afefd2f982df9f98caee",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "9ed1494d65bc3f82866b",
+    "url": "/Clicky-Game/static/css/main.8ff7aa1f.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Clicky-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "9ed1494d65bc3f82866b",
+    "url": "/Clicky-Game/static/js/main.0d1414eb.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "fed31b7cbd7cf4eba30aa0907b9e7a20",
+    "url": "/Clicky-Game/static/media/bananas.fed31b7c.png"
+  }
+]);
